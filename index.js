6da7bb12016a75364c5098df5c24bbe5a07e7155
MBURU <wawerumburu157@gmail.com>
2024-09-30 16:27:20 +0300

    functions

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(pickupLocation) {
+    const hqLocation = 42;
+    return Math.abs(pickupLocation - hqLocation);
+  }
+  
+ 
+  function distanceFromHqInFeet(pickupLocation) {
+    const blocks = distanceFromHqInBlocks(pickupLocation);
+    return blocks * 264; 
+  }
+  
+
+  function distanceTravelledInFeet(start, destination) {
+    const blocks = Math.abs(destination - start);
+    return blocks * 264;
+  }
+  
+  
+  function calculatesFarePrice(start, destination) {
+    const distanceInFeet = distanceTravelledInFeet(start, destination);
+    
+    if (distanceInFeet <= 400) {
+      return 0; 
+    } else if (distanceInFeet > 400 && distanceInFeet <= 2000) {
+      return (distanceInFeet - 400) * 0.02; 
+        } else if (distanceInFeet > 2000 && distanceInFeet <= 2500) {
+      return 25;
+    } else {
+      return 'cannot travel that far'; 
+    }
+  }
+  
